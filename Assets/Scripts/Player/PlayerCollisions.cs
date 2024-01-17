0f5d1febf80c0cecf4aac87ab6dcb97d65afaa96
Antonin Masopust <purple.dragon@email.cz>
2024-01-17 22:27:26 +0100

    Add collisions to snake

--- Assets/Scripts/Player/PlayerCollisions.cs
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeCreateCollider : MonoBehaviour
+public class PlayerCollisions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,14 @@ public class SnakeCreateCollider : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision");
+        if (collision.collider.tag == "Enemy")
+        {
+            print("enemy");
+        }
     }
 }
--- Assets/Scripts/Snake/SnakeLine.cs
+++ Assets/Scripts/Snake/SnakeLine.cs
@@ -12,6 +12,8 @@ public class SnakeLine : MonoBehaviour
 
     private Vector3[] segVel;
     private Vector3[] segPos;
+
+    private EdgeCollider2D edgeCollider;
     
 
     // Start is called before the first frame update
@@ -21,19 +23,29 @@ public class SnakeLine : MonoBehaviour
         lineRenderer.positionCount = length;
         segPos = new Vector3[length];
         segVel = new Vector3[length];
+
+        edgeCollider = GetComponentInChildren<EdgeCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 worldSegpos;
+
         segPos[0] = target.position;
+        Vector2[] edges = new Vector2[length];
+        worldSegpos = transform.InverseTransformPoint(segPos[0]);
+        edges[0] = new Vector2(worldSegpos.x, worldSegpos.y);
 
         for(int i = 1; i < length; i++)
         {
             Vector3 targetPos = segPos[i - 1] + (segPos[i] - segPos[i - 1]).normalized * targetDist;
             segPos[i] = Vector3.SmoothDamp(segPos[i], targetPos, ref segVel[i],smoothSpeed);
+            worldSegpos = transform.InverseTransformPoint(segPos[i]);
+            edges[i] = new Vector2(worldSegpos.x, worldSegpos.y);
 		}
 
         lineRenderer.SetPositions(segPos);
+        edgeCollider.points = edges;
     }
 }
