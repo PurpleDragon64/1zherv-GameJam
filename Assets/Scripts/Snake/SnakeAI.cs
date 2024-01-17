84a3cda6cb4353c88f20147e3f9dc2626b26762a
Antonin Masopust <purple.dragon@email.cz>
2024-01-17 17:34:30 +0100

    Implement snake AI

--- Assets/Scripts/Snake/SnakeAI.cs
+++ Assets/Scripts/Snake/SnakeAI.cs
@@ -5,20 +5,30 @@ using UnityEngine.AI;
 
 public class SnakeAI : MonoBehaviour
 {
+    // debug
     public enum EState {PATROL, CHASE, ALERT};
     public EState state;
-    public GameObject chaseTarget;
 
-    public float rotateSpeed = 25;
+    public GameObject chaseTarget;
+    public Transform[] waypoints;
+    private int waypointIdx;
+    private Transform waypoint;
+    private int waypointsCount;
+
+
     public float patrolSpeed = 5;
     public float chaseSpeed = 10;
+    public float patrolAcceleration = 6;
+    public float chaseAcceleration = 13;
+    public float patrolRotateSpeed = 5;
+    public float chaserotateSpeed = 25;
+    private float rotateSpeed = 5;
 
     public FieldOfView fov;
     [Range(0,360)]
     public float defaultFOVAngle;
 
     private Vector3 targetPos;
-    private Vector2 direction;
 
     NavMeshAgent agent;
 
@@ -29,6 +39,12 @@ public class SnakeAI : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
+        waypointIdx = 0;
+        waypoint = waypoints[waypointIdx];
+        waypointsCount = waypoints.Length;
+        targetPos = waypoint.position;
+
+
         state = EState.PATROL;
         fov.FOVAngle = defaultFOVAngle;
     }
@@ -36,35 +52,48 @@ public class SnakeAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(fov.targetDetected) {
-			targetPos = chaseTarget.transform.position;
-            state = EState.CHASE;	
-		}
+        if (fov.targetDetected)
+        {
+            targetPos = chaseTarget.transform.position;
+            state = EState.CHASE;
+            agent.speed = chaseSpeed;
+            agent.acceleration = chaseAcceleration;
+            rotateSpeed = chaserotateSpeed;
+        }
 
-        float angle;
-        Quaternion rotation;
+        if (agent.remainingDistance < 0.5f)
+        {
+            if (state == EState.PATROL)
+            {
+                waypoint = waypoints[++waypointIdx % waypointsCount];
+            }
+            else
+            {
+                state = EState.PATROL;
+                agent.speed = patrolSpeed;
+                agent.acceleration = patrolAcceleration;
+                rotateSpeed = patrolRotateSpeed;
+            }
 
-        switch (state) {
-            case EState.CHASE:
-                if(transform.position != targetPos) { 
-					// rotate towards target
-					direction = targetPos - transform.position;
-					angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-					rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            targetPos = waypoint.position;
+        }
 
-					transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed *Time.deltaTime);
+        RotateSnake();
+        MoveSnake();
+    }
 
-					// move towards target
-				    transform.position = Vector3.MoveTowards(transform.position, targetPos, chaseSpeed * Time.deltaTime);
-				}
-                else {
-                    state = EState.ALERT;	
-				}
-                break;
-            case EState.ALERT:
-                    fov.FOVAngle = 360;
-                    
-                break;	
-		}
+    void RotateSnake()
+    {
+        Vector3 velocity = agent.velocity;
+
+        float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+    }
+
+    void MoveSnake()
+    {
+        agent.SetDestination(targetPos);
     }
 }
